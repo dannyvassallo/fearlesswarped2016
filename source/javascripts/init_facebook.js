4d272c1ccd25a382860364d23fa7f6a147cbe1af
dannyvassallo <danielvassallo87@gmail.com>
2016-05-12 15:15:59 -0400

    updated url

--- source/javascripts/init_facebook.js
+++ source/javascripts/init_facebook.js
@@ -35,7 +35,7 @@
       // SHARE INFO
       method: 'feed',
       name: 'Welcome to the #FearlessSummer Olympics', // name of the product or content you want to share
-      link: 'http://fearlesssummer.com', // link back to the product or content you are sharing
+      link: 'http://fearlesssummer.fearlessrecords.com/', // link back to the product or content you are sharing
       picture: 'https://s3.amazonaws.com/myfangate.com/fearlesswarped2016/shareimage.jpg', // path to an image you would like to share with this content
       caption: 'The Fearless Family wants to send you and a friend to a Van\'s Warped Tour Near You!', // caption
       description: 'Enter the #FearlessSummer sweepstakes to win a pair of tickets to a Vans Warped Tour date near you!' // description of your product or content
